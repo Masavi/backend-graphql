7f542aa8e8ed694dce19bd1dee50f7911076468b
maui <maui@devf.mx>
2019-05-03 19:50:08 -0500

    hash user password from schema event

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new mongoose.Schema({
     first_name: { 
@@ -22,6 +24,27 @@ const UserSchema = new mongoose.Schema({
         enum: ["Male", "Female", "Undefined"],
         default: "Undefined",
     }
+});
+
+UserSchema.pre('save', function(next){
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 const Users = mongoose.model("Users", UserSchema);
